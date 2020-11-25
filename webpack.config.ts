69d582fcb4c549bc8671788eeee2912d0eebd266
murakami <w18191263056@yahoo.co.jp>
2020-11-25 21:25:46 +0800

    webpack tab space

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,49 +1,47 @@
 import path from 'path'
 import webpack from 'webpack'
-import 'webpack-dev-server'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import 'webpack-dev-server'
 
 const config: webpack.Configuration = {
-    entry: './src/index.tsx',
-    mode: 'development',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
+  entry: './src/index.tsx',
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devtool: 'eval-cheap-module-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@rxjs-react': path.resolve(__dirname, 'rxjs-react'),
     },
-    devtool: 'eval-cheap-module-source-map',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@rxjs-react': path.resolve(__dirname, 'rxjs-react'),
-        },
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'),
-    }), new webpack.HotModuleReplacementPlugin(), ],
-    module: {
-        rules: [{
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: [['@babel/preset-env', {
-                        useBuiltIns: 'entry',
-                        corejs: 3
-                    }], '@babel/preset-react', '@babel/preset-typescript', ]
-                },
-            }]
-        },
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8080,
-        historyApiFallback: true,
-    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'] } }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+  }
 }
 
 export default config
